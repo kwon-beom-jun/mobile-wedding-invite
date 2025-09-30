b0d81fd05b9cce288ac92a81b823a67d3d17c5a6
kwon-beom-jun <qjawns0618@naver.com>
2025-09-30 22:39:01 +0900

    docs upload

--- wedding-mobile-frontend/vite.config.ts
+++ wedding-mobile-frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/wedding-mobile-frontend/', // 레포 이름으로 설정
+  build: {
+    outDir: '../docs' // dist 대신 docs로 출력
+  }
 })
