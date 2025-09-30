4e5bf2e7073590c0eed2f48916360283848754c9
kwon-beom-jun <qjawns0618@naver.com>
2025-09-30 23:00:33 +0900

    index.html 재수정

--- wedding-mobile-frontend/vite.config.ts
+++ wedding-mobile-frontend/vite.config.ts
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wedding-mobile-frontend/', // 프로젝트 페이지 경로
+  base: '/mobile-wedding-invite/', // GitHub Pages 프로젝트 경로
   build: {
-    outDir: '../docs' // 레포 내부 docs로 출력 (GitHub Pages: main/docs)
+    outDir: '../docs' // 레포 내부 docs
   }
 })
