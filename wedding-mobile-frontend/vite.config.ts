d7040039d3b13f5e103bc604040444bc4b678fac
kwon-beom-jun <qjawns0618@naver.com>
2025-09-30 22:53:21 +0900

    빌드 폴더 재수정

--- wedding-mobile-frontend/vite.config.ts
+++ wedding-mobile-frontend/vite.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   plugins: [react()],
   base: '/wedding-mobile-frontend/', // 프로젝트 페이지 경로
   build: {
-    outDir: 'docs' // 레포 내부 docs로 출력 (GitHub Pages: main/docs)
+    outDir: '../docs' // 레포 내부 docs로 출력 (GitHub Pages: main/docs)
   }
 })
